954df4c6dfb74c77659520d97e276bfaa5e5db98
Andrzej Blachuta <113551896+Jyndrek-06@users.noreply.github.com>
2023-02-15 20:28:05 +0000

    We did from the slideshow

--- main.py
+++ main.py
@@ -1,2 +1,33 @@
 # remember to fork this and push to github
 
+# f = open('test.txt', "r")
+# print(f.readlines())
+# f.close()
+
+# with open('test.txt', 'r') as f: 
+#   f_contents = f.readlines()
+#   print(f_contents, end='')
+
+# employee_file = open("employees.txt", "r")
+# print(employee_file.readlines())
+# employee_file.close()
+
+# employee_file = open("employees.txt", "r")
+# print(employee_file.readlines()[1])
+# employee_file.close()
+
+# employee_file = open("employees.txt", "r")
+# for employee in employee_file.readlines():
+#   print(employee)
+# employee_file.close()
+
+with open('readme.txt', 'w') as f:
+  f.write('readme')
+
+  # this overwrites the original content
+with open('readme.txt', 'w') as f:
+  f.write('I dont care')
+
+  # this appends to the oroginal content 
+with open('readme.txt', 'a') as f:
+  f.write('I dont care 3xxxxxx')
